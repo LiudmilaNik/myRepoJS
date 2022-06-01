ddf2aaf0f495a7bbdff8bc13ba8cc59174e5666d
Liudmila Bochkovskaia <LudaB74@mail.ru>
2022-06-01 12:07:36 +0300

    исправлены ошибки второго задания

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let title = "Projekt";
 console.log(typeof title);
+
 let screens = "Простые, Сложные, Интерактивные";
 console.log(screens.length);
-console.log(screens.toLowerCase());
-console.log(screens.split(", "));
+console.log(screens.toLowerCase().split(", "));
+
 let screenPrice = 20;
-console.log(20);
+console.log(screenPrice);
+
 let rollback = 10;
-console.log((50 * 10) / 100);
+
 let fullPrice = 50;
-console.log(50);
+console.log(fullPrice);
 console.log(typeof fullPrice);
+
 let adaptive;
 console.log(typeof adaptive);
+
+console.log((fullPrice * rollback) / 100);
